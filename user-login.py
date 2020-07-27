b42006cff04b5796b3c4efe3c5aabe30e60cfd0e
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-27 11:43:04 -0600

    commit

---
+++ user-login.py
@@ -0,0 +1,11 @@
+print('User Login')
+
+# input
+username = input('Username:')
+password = input('Password:')
+
+# test input & output login attempt result
+if (username == 'seragelgamal') and (password == 'awesome123'):
+    print('Login Successful')
+else:
+    print('Login Failed')
